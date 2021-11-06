e03e919ccf14e81f9c9691c46a33aa2b471d73ea
aaldn <aladdine.abdou.sio@gmail.com>
2021-11-06 22:19:38 +0100

    Securisation des opérations du visiteur sur les fiches de frais
    Seule la fiche frais du mois courant peut subir des modifications

--- src/Controller/VisiteurController.php
+++ src/Controller/VisiteurController.php
@@ -94,56 +94,48 @@ class VisiteurController extends AbstractController
         ]);
     }
 
-    public function consulterDetailFicheFrais(int $idFicheFrais, Request $request, EntityManagerInterface $em): Response
+    public function consulterDetailFicheFrais(int $idFicheFrais, EntityManagerInterface $em): Response
     {
-        $visiteur = $this->getUser();
-
         $ficheFrais = $em->getRepository(Fichefrais::class)->findFichefrais($idFicheFrais);
         $fraisHorsForfaits = $em->getRepository(Lignefraishorsforfait::class)->findByFichefrais($ficheFrais);
         $ligneFraisForfaits = $em->getRepository(Lignefraisforfait::class)->findByFichefrais($ficheFrais);
         $fraisForfaits = $em->getRepository(Fraisforfait::class)->findAllAsc();
 
-        // $ficheFraisInstance = new Fichefrais();
-        // $formFicheFrais = $this->createForm(FichefraisType::class, $ficheFraisInstance);
-        // $formFicheFrais->handleRequest($request);
+        $ficheFraisInstance = new Fichefrais();
+        $formFicheFrais = $this->createForm(FichefraisType::class, $ficheFraisInstance);
 
-        // $fraisHorsForfaitInstance = new Lignefraishorsforfait();
-        // $formFraisHorsForfait = $this->createForm(LignefraishorsforfaitType::class, $fraisHorsForfaitInstance);
-        // $formFraisHorsForfait->handleRequest($request);
-
-        // if ($formFicheFrais->isSubmitted() && $formFicheFrais->isValid()) {
-        //     $i = 0;
-        //     foreach ($formFicheFrais->getData()->getLignefraisforfaits() as $ligneFraisForfaitInput) {
-        //         $ligneFraisForfaits[$i]->setQuantite($ligneFraisForfaitInput->getQuantite());
-        //         $em->persist($ligneFraisForfaits[$i]);
-        //         $i++;
-        //     }
-        //     $em->flush();
-
-        //     return $this->redirectToRoute('consulter_detail_fiche_frais', compact('idFicheFrais'));
-        // }
-
-        // if ($formFraisHorsForfait->isSubmitted() && $formFraisHorsForfait->isValid()) {
-        //     $fraisHorsForfaitInstance->setIdvisiteur($ficheFrais->getIdvisiteur()->getId());
-        //     $fraisHorsForfaitInstance->setMois($ficheFrais->getMois());
-        //     $fraisHorsForfaitInstance->setIdfichefrais($ficheFrais);
-        //     $em->persist($formFraisHorsForfait->getData());
-        //     $em->flush();
-
-        //     return $this->redirectToRoute('consulter_detail_fiche_frais', compact('idFicheFrais'));
-        // }
+        $fraisHorsForfaitInstance = new Lignefraishorsforfait();
+        $formFraisHorsForfait = $this->createForm(LignefraishorsforfaitType::class, $fraisHorsForfaitInstance);
 
         return $this->render('visiteur/consulter_detail_fiche_frais.html.twig', [
             'controller_name' => 'VisiteurController',
-            // 'formFraisHorsForfait' => $formFraisHorsForfait->createView(),
-            // 'formFicheFrais' => $formFicheFrais->createView(),
+            'formFraisHorsForfait' => $formFraisHorsForfait->createView(),
+            'formFicheFrais' => $formFicheFrais->createView(),
             'ficheFrais' => $ficheFrais,
             'ligneFraisForfaits' => $ligneFraisForfaits,
             'fraisHorsForfaits' => $fraisHorsForfaits,
             'fraisForfaits' => $fraisForfaits
         ]);
     }
-    //     return $this->redirectToRoute('consulter_detail_fiche_frais', compact('id'));
+
+    public function supprimerFraisHorsForfait(Request $request, Lignefraishorsforfait $lignefraishorsforfait): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $visiteur = $this->getUser();
+        $ficheFraisCourante = $em->getRepository(Fichefrais::class)->findFichefraisCourante($visiteur);
+
+        if ($lignefraishorsforfait->getMois() != $ficheFraisCourante->getMois()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $lignefraishorsforfait->getId(), $request->request->get('supprimer_fraishorsforfait_token'))) {
+
+            $em->remove($lignefraishorsforfait);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('saisir_fiche_frais', [], Response::HTTP_SEE_OTHER);
+    }
 
     private function creerFichefrais($ficheFrais, $fraisForfaits, $visiteur)
     {
@@ -164,16 +156,5 @@ class VisiteurController extends AbstractController
         $em->flush();
 
         return $ficheFrais;
-    }
-
-    public function supprimerFraisHorsForfait(Request $request, Lignefraishorsforfait $lignefraishorsforfait): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $lignefraishorsforfait->getId(), $request->request->get('supprimer_fraishorsforfait_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($lignefraishorsforfait);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('saisir_fiche_frais', [], Response::HTTP_SEE_OTHER);
     }
 }
