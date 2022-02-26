886286b64a49f5710f2c207d8229ebe864f8bb21
aaldn <aladdine.abdou.sio@gmail.com>
2022-02-26 14:54:59 +0100

    Correction du script de peuplement de la base de données

--- script/database.sh
+++ script/database.sh
@@ -2,5 +2,5 @@
 
 # Peuplement de la base de données (+ fixtures)
 docker exec gsb_frais_www sh -c "symfony console doctrine:fixtures:load -n"
-docker exec gsb_frais_db sh -c "mariadb -e \"use gsb_frais; source sql/gsb_insert_tables.sql;\""
+docker exec gsb_frais_db sh -c "mysql -e \"use gsb_frais; source gsb_insert_tables.sql;\""
 docker exec gsb_frais_www sh -c "symfony console doctrine:fixtures:load --append"
